9314eaa2510a76984f50ec93dfce474fec846bbc
Eric Dahl <dahlor@gmail.com>
2021-01-15 20:23:50 -0500

    G Y P S Y

--- module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/LinearConvert.java
+++ module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/LinearConvert.java
@@ -1,8 +1,37 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class LinearConvert {
 
 	public static void main(String[] args) {
+
+		Scanner userInput  = new Scanner(System.in);
+		
+		String lengthInput = "";
+		String freedomUnits = "";
+		double lengthMeters = 0;
+		double lengthFeet = 0;
+
+		while(true) {
+		System.out.println("Please enter the length to be converted: " );
+		lengthInput = userInput.nextLine();
+		
+		double convertedNum = Double.parseDouble(lengthInput);
+		
+		System.out.println("Would you like the result converted to (M)eters or (F)eet? " );
+		freedomUnits = userInput.nextLine();
+		
+		if (freedomUnits.equals("M") || freedomUnits.equals("m"))	{
+			lengthMeters = (convertedNum * 0.3048);
+			System.out.println("Once converted, " + lengthInput + " feet becomes " + lengthMeters + " meters.");
+		}
+		if (freedomUnits.equals("F") || freedomUnits.equals("f"))	{
+			lengthFeet = (convertedNum * 3.2808399);
+			System.out.println("Once converted, " + lengthInput + " meters becomes " + lengthFeet + " feet.");
+		}
+
+		}
 
 	}
 
--- module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/TempConvert.java
+++ module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/TempConvert.java
@@ -1,8 +1,37 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class TempConvert {
 
 	public static void main(String[] args) {
+		
+		Scanner userInput  = new Scanner(System.in);
+		
+		String tempInput = "";
+		String freedomUnits = "";
+		int tempCelsius = 0;
+		int tempFahrenheit = 0;
+
+		while(true) {
+		System.out.println("Please enter the temperature to be converted: " );
+		tempInput = userInput.nextLine();
+		
+		int convertedNum = Integer.parseInt(tempInput);
+	
+		System.out.println("Would you like the result converted to (C)elsius or (F)ahrenheit? " );
+		freedomUnits = userInput.nextLine();
+		
+		if (freedomUnits.equals("C") || freedomUnits.equals("c"))	{
+			tempCelsius = ((convertedNum-32) * 5/9);
+			System.out.println("Once converted, " + tempInput + "°F becomes " + tempCelsius + "°C.");
+		}
+		if (freedomUnits.equals("F") || freedomUnits.equals("f"))	{
+			tempFahrenheit = ((convertedNum * 9/5) + 32);
+			System.out.println("Once converted, " + tempInput + "°C becomes " + tempFahrenheit + "°F.");
+		}
+
+		}
 
 	}
 
