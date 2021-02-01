17b590784225109ad27853341f7b4a06fbaa594c
Eric Dahl <dahlor@gmail.com>
2021-02-01 14:43:03 -0500

    Mad Hatter Maven & Caterpillar Code

--- module-1/16_FileIO_Reading_in/student-exercise/java/src/main/java/com/techelevator/WordSearch.java
+++ module-1/16_FileIO_Reading_in/student-exercise/java/src/main/java/com/techelevator/WordSearch.java
@@ -1,10 +1,59 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class WordSearch {
 
-	public static void main(String[] args) {
-	
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		// Print dat sexy banner
+		printApplicationBanner();
+		
+		// Instantiate my jabberwocky
+		File aliceFile = new File("./alices_adventures_in_wonderland.txt");
+		
+		// Instantiate a Scanner object for the jabberwocky
+		Scanner aliceReader = new Scanner(aliceFile);
+		
+		// Getting all the inputs inputted
+		Scanner searchInput = new Scanner(System.in);							
+		System.out.print("What is the search word you are looking for? \n");	
+		String searchedWord = searchInput.nextLine();						  
+		System.out.print("Should the search be case sensitive? (Y/N) \n");	
+		String caseSensitive = searchInput.nextLine();						
+		searchInput.close();
+		System.out.print("\nSearch Results:\n\n");
 
+		// Declare my line counter
+		int counter = 1;
+		
+		// If/Else for what to do depending on the case sensitivity answer.
+		if (caseSensitive.toUpperCase() == "Y") {
+			// Look for the word in the text
+			while(aliceReader.hasNext()) { 					 
+				String aLine = aliceReader.nextLine().toLowerCase();
+				if (aLine.contains(searchedWord.toLowerCase())){
+					System.out.println(counter + ") " + aLine);		 
+					}
+				counter++;
+			}			
+		} else {
+			// Look for the word in the text
+			while(aliceReader.hasNext()) { 					 
+				String aLine = aliceReader.nextLine();
+				if (aLine.contains(searchedWord)){
+					System.out.println(counter + ") " + aLine);		 
+					}
+				counter++;
+		}			
 	}
-
+}
+	private static void printApplicationBanner() {
+		System.out.println("****************************************************");
+		System.out.println("*  E A T  M E   D R I N K  M E   S E A R C H  M E  *");
+		System.out.println("****************************************************");
+		System.out.println();
+	}	
 }
--- module-1/16_FileIO_Reading_in/student-lecture/java/src/main/java/com/techelevator/myFileReadingCode/MyFileReader.java
+++ module-1/16_FileIO_Reading_in/student-lecture/java/src/main/java/com/techelevator/myFileReadingCode/MyFileReader.java
@@ -1,8 +1,48 @@
 package com.techelevator.myFileReadingCode;
 
+import java.io.File;	  // include the code to support Java file i/o
+import java.io.FileNotFoundException; //include the code to support FileNotFoundExceptions
+import java.util.Scanner; // include the code to support the Java Scanner
+
 public class MyFileReader {
 
-	public static void main(String[] args) {
+	// throws tell the compiler you know your code might throw an exception
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		// Read the data from a file containing lines of numbers separated by commas
+		// Display the line we read from the file
+		// Sum the numbers
+		// Average the numbers
+		// Print the sum and average
+		
+		double sum = 0;			// Hold the sum of the numbers in a line from the file
+		double average = 0;		// Hold the average of the numbers in a line from the file
+		
+		// Instantiate a File object for the file containing our data
+		
+		File theFile = new File("./data/numbers.txt");
+		
+		// Instantiate a Scanner object for the file
+		
+		Scanner fileReader = new Scanner(theFile);
+		
+		// Loop through the file one line at a time
+		while(fileReader.hasNext()) { 					  	   // As long as the file has a next line to read
+			String aLine = fileReader.nextLine(); 		  	   // Get the next line from the file
+			System.out.println("The line read: " + aLine); 	   // The line
+			String[] theNumbers = aLine.split(",");		   	   // Split the line based on a comma
+			
+			sum = 0;										   // Reset sum to clear any old value
+			
+			for (int i = 0; i < theNumbers.length; i++) {      // Loop through the String number values one at a time
+				int aNumber = Integer.parseInt(theNumbers[i].trim()); // 		Convert the current element in theNumbers to an int
+				sum += aNumber;								   //		add the number to the sum
+			}
+			average = sum / theNumbers.length;
+			System.out.println("Sum: " + sum + " avg " + average);
+		}
+		fileReader.close(); // Close the Scanner to avoid a resource leak
+		// Sum the numbers
 		
 }
 }
