9f5adabe24dc7bcdc1acbafbb94716bfe3fd5384
Eric Dahl <dahlor@gmail.com>
2021-02-12 15:05:59 -0500

    National Park Campsite Completed

--- module-2/05_Week_1_Review/student-exercise/step-4.sql
+++ module-2/05_Week_1_Review/student-exercise/step-4.sql
@@ -1,11 +1,41 @@
 -- select the park name, campground name, open_from_mm, open_to_mm & daily_fee ordered by park name and then campground name
 
+select park.name as park_name, campground.name as campground_name, open_from_mm, open_to_mm, daily_fee
+from site
+                inner join
+                campground
+        on site.campground_id = campground.campground_id
+                join
+                park
+        on campground.park_id = park.park_id
+group by park.name, campground.name, open_from_mm, open_to_mm, daily_fee
+order by park.name, campground.name
+;
 
 -- select the park name and the total number of campgrounds for each park ordered by park name
 
-
+select park.name, count(campground_id)
+from campground
+                inner join
+                park
+        on campground.park_id = park.park_id
+group by park.name
+order by park.name
+;
 
 -- select the park name, campground name, site number, max occupancy, accessible, max rv length, utilities where the campground name is 'Blackwoods'
+
+select park.name as park_name, campground.name as campground_name, site.site_id, max_occupancy, accessible, max_rv_length, utilities
+from site
+                inner join
+                campground
+        on site.campground_id = campground.campground_id
+                join
+                park
+        on campground.park_id = park.park_id
+where campground.name = 'Blackwoods'        
+group by park.name, campground.name, site.site_id, max_occupancy, accessible, max_rv_length, utilities
+;
 
 /*
   select park name, campground, total number of sites (column alias 'number_of_sites') ordered by park
@@ -20,10 +50,24 @@
   -------------------------------------------------
 */
 
+select park.name, campground.name, count(site_id) as number_of_sites
+from site
+                inner join
+                campground
+        on site.campground_id = campground.campground_id
+                join
+                park
+        on campground.park_id = park.park_id
+group by park.name, campground.name
+order by park.name, count(site_id) desc
+;
 
 -- select site number, reservation name, reservation from and to date ordered by reservation from date
 
-
+select site_id, name, from_date, to_date
+from reservation
+order by from_date
+;
 
 /*
   select campground name, total number of reservations for each campground ordered by total reservations desc
@@ -38,6 +82,17 @@
   -------------------------------------------------
 */
 
-
-
-
+select campground.name as campground_name, count(reservation_id) as total_reservations
+from reservation
+                inner join
+                site
+        on reservation.site_id = site.site_id
+                join
+                campground
+        on site.campground_id = campground.campground_id
+                join
+                park
+        on campground.park_id = park.park_id
+group by campground.name
+order by count(reservation_id) desc
+;
--- module-2/05_Week_1_Review/student-exercise/step-5.sql
+++ module-2/05_Week_1_Review/student-exercise/step-5.sql
@@ -10,12 +10,36 @@
   ------------------------------
 */
 
+begin transaction;
+
+insert into park
+(name, location, establish_date, area, visitors, description)
+values('Ohiopyle State Park', 'Pennsylvania', '1965-01-01', 19052, 1000000, 'Ohiopyle State Park is a Pennsylvania state park on 19,052 acres in Dunbar, Henry Clay and Stewart Townships, Fayette County, Pennsylvania in the United States. The focal point of the park is the more than 14 miles of the Youghiogheny River Gorge that passes through the park.')
+;
+
+select * from park;
+
+commit;
+
+rollback;
 
 /*
   STEP TWO: You just found out that there was an error with the park data. Please update the park visitors to 1.5 million anually.
 
 */
 
+begin transaction;
+
+update park
+set visitors = 1500000
+where park_id = 4
+;
+
+select * from park;
+
+commit;
+
+rollback;
 
 /*
  STEP THREE: Insert new campground with the following data:
@@ -28,6 +52,18 @@
   ------------------------------------------------------------
 */
 
+begin transaction;
+
+insert into campground
+(park_id, name, open_from_mm, open_to_mm, daily_fee)
+values(4, 'Youghiogheny', '01', '12', '95.00')
+;
+
+select * from campground where park_id = '4';
+
+commit;
+
+rollback;
 
 /*
  STEP FOUR: Insert 3 new sites with the following data:
@@ -41,6 +77,28 @@
 
 */
 
+begin transaction;
+
+insert into site
+(site_number, campground_id)
+values(623, 8)
+;
+
+insert into site
+(site_number, campground_id)
+values(624, 8)
+;
+
+insert into site
+(site_number, campground_id)
+values(625, 8)
+;
+
+select * from site where campground_id = 8;
+
+commit;
+
+rollback;
 
 /*
  STEP FIVE: Insert 3 reservations, 1 for each site with the following data:
@@ -52,15 +110,60 @@
 
 */
 
+begin transaction;
+
+insert into reservation
+(site_id, name, from_date, to_date)
+values('623', 'Wayne Family', (select (current_date + interval '10 days')), (select (current_date + interval '20 days')))
+;
+
+insert into reservation
+(site_id, name, from_date, to_date)
+values('624', 'Parker Family', (select (current_date + interval '11 days')), (select (current_date + interval '20 days')))
+;
+
+insert into reservation
+(site_id, name, from_date, to_date)
+values('625', 'Kent Family', (select (current_date + interval '12 days')), (select (current_date + interval '20 days')))
+;
+
+select * from reservation where site_id > 622;
+
+commit;
+
+rollback;
 
 /*
  STEP SIX: The Wayne Family called and asked if they could change their reservation to today. Update the from_date to today and the to_date to a week from today.
 
  */
 
+begin transaction;
+
+update reservation
+set from_date  = (select (current_date)), to_date = (select (current_date + interval '7 days'))
+where name = 'Wayne Family'
+;
+
+select * from reservation where name = 'Wayne Family';
+
+commit;
+
+rollback;
 
 /*
  STEP SEVEN: The Kent family called and they would like to cancel their reservation. Delete the reservation for Kent Family.
 
 */
 
+begin transaction;
+
+delete from reservation
+where name = 'Kent Family'
+;
+
+select * from reservation where name = 'Kent Family';
+
+commit;
+
+rollback;
